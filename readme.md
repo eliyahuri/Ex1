1048bf8bf1f52e99ac8b1ddf09ce9353a3a9d9cb
eliyahuri <tkhvu1236@gmail.com>
2024-12-11 10:59:35 +0200

    Enhance Ex1 solution with detailed README and initial implementation of number formatting functions

--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
+
 # Ex1
+
+hi this is my solution to assignment 1 in the java course of ariel university
+
+the course didn't ask to use maven but i couldn't run junit any other way
+
+added is an image depicting me trying to make junit work
+
+![Trying to make JUnit work](https://i.imgflip.com/4co4m2.png)
--- src/test/java/Ex1Test.java
+++ src/test/java/Ex1Test.java
@@ -1,16 +1,53 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
-import assignments.ex1.Ex1Main;
+import assignments.ex1.Ex1;
 
-
+/**
+ * This JUnit class represents a very partial test class for Ex1. Make sure you
+ * complete all the needed JUnits
+ */
 public class Ex1Test {
 
     @Test
-    public void testIsEven() {
-        Ex1Main main = new Ex1Main();
-        assertTrue(main.isEven(2),"2 is even");
-        assertFalse(main.isEven(3));
+    void computeNumberTest() {
+        String s2 = "1011b2";
+        int v = Ex1.number2Int(s2);
+        assertEquals(v, 11);
+        String s10 = "1011bA";
+        v = Ex1.number2Int(s10);
+        s2 = Ex1.int2Number(v, 2);
+        int v2 = Ex1.number2Int(s2);
+        assertEquals(v, v2);
+        assertTrue(Ex1.equals(s10, s2));
     }
+
+    @Test
+    void isBasisNumberTest() {
+        String[] good = {"1", "1b2", "01b2", "123bA", "ABbG", "0bA"};
+        for (int i = 0; i < good.length; i = i + 1) {
+            boolean ok = Ex1.isNumber(good[i]);
+            assertTrue(ok);
+        }
+        String[] not_good = {"b2", "2b2", "1G3bG", " BbG", "0bbA", "abB", "!@b2", "A", "1bb2"};
+        for (int i = 0; i < not_good.length; i = i + 1) {
+            boolean not_ok = Ex1.isNumber(not_good[i]);
+            assertFalse(not_ok);
+        }
+    }
+
+    @Test
+    void int2NumberTest() {
+        // implement this test
+    }
+
+    @Test
+    void maxIndexTest() {
+        // implement this test
+    }
+
+    // Add additional test functions - test as much as you can.
 }
